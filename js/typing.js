54034851044751190e75aeda288d1ef644a737a3
Savannah Ostrowski <savannahostrowski@gmail.com>
2016-01-06 11:09:43 -0500

    overhaul

---
+++ js/typing.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $(".typed").typed({
+        strings: ["a developer^1000",
+        		  "an amateur rock climber^1000",
+        		  "a gal who loves to cook^1000",
+        		  "obsessed with visualizing data^1000",
+        		  "a (soon to be) uWaterloo grad^1000"],
+        typeSpeed: 50,
+        cursorChar: "",
+        loop: true,
+    });
+  });
